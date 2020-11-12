d588e5de70d34be506b718ef42594b2ecaf73e69
lukas <dr.oste129@gmail.com>
2020-11-12 17:15:52 +0100

    Some Rusty Math

--- src/print.rs
+++ src/print.rs
@@ -6,7 +6,23 @@ pub fn run() {
     println!("{} is from {}", "Lukas", "Soest");
 
     // Positional Arguments
-    println!("{0} is from {1} and {0} likes to {2}", "Lukas", "Soest", "code");
+    println!("{0} is from {1} and {0} likes to {2}",
+             "Lukas",
+             "Soest", "code"
+    );
     // Named Arguments
-    println!("{name} likes to play {activity}", name = "Lukas", activity = "ALL THE DRINKING GAMES!!!111")
+    println!("{name} likes to play {activity}",
+             name = "Lukas",
+             activity = "ALL THE DRINKING GAMES!!!111"
+    );
+
+    // Placeholder traits
+    println!("Binary: {:b}, Hex: {:x}, Octal: {:o}", 10, 10, 10);
+
+    // Placeholder for debug trait
+    println!("{:?}", (12, true, "hello"));
+
+    // Basic math
+    println!("10 + 10 = {}, wtf thats not the result of 10 +10?", 10 + 100);
+
 }
