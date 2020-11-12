741495a9f749a8e3cb1486229ec74c875eb96734
lukas <dr.oste129@gmail.com>
2020-11-12 17:02:17 +0100

    Some Rusty Lessons

---
+++ src/print.rs
@@ -0,0 +1,12 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from the print.rs file");
+
+    // Basic Formatting
+    println!("{} is from {}", "Lukas", "Soest");
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}", "Lukas", "Soest", "code");
+    // Named Arguments
+    println!("{name} likes to play {activity}", name = "Lukas", activity = "ALL THE DRINKING GAMES!!!111")
+}
